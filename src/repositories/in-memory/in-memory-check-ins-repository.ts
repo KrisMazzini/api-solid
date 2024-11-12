340dcde7f0eaf41c786a6ade71041eaa0b20ff9a
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2024-11-12 20:40:19 -0300

    feat: implement 'check-in' use case

---
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -0,0 +1,39 @@
+import { randomUUID } from 'node:crypto'
+
+import { CheckIn, Prisma } from '@prisma/client'
+import dayjs from 'dayjs'
+
+import { CheckInsRepository } from '../check-ins-repository'
+
+export class InMemoryCheckInsRepository implements CheckInsRepository {
+  public checkIns: CheckIn[] = []
+
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn: CheckIn = {
+      id: randomUUID(),
+      user_id: data.user_id,
+      gym_id: data.gym_id,
+      validated_at: data.validated_at ? new Date(data.validated_at) : null,
+      created_at: new Date(),
+    }
+
+    this.checkIns.push(checkIn)
+
+    return checkIn
+  }
+
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    const checkInOnSameDate = this.checkIns.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at)
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+      return checkIn.user_id === userId && isOnSameDate
+    })
+
+    return checkInOnSameDate ?? null
+  }
+}
---
+++ src/use-cases/check-in.test.ts
@@ -0,0 +1,133 @@
+import { randomUUID } from 'node:crypto'
+
+import { Decimal } from '@prisma/client/runtime/library'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+
+import { CheckInUseCase } from './check-in'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let gymsRepository: InMemoryGymsRepository
+let sut: CheckInUseCase
+
+let userId: string
+let gymId: string
+
+let userLatitude: number
+let userLongitude: number
+
+describe('Use Case: Get User Profile', async () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    gymsRepository = new InMemoryGymsRepository()
+
+    sut = new CheckInUseCase(checkInsRepository, gymsRepository)
+
+    userId = randomUUID()
+    gymId = randomUUID()
+
+    userLatitude = -21.7601687
+    userLongitude = -43.3484179
+
+    gymsRepository.gyms.push({
+      id: gymId,
+      title: 'JavaScript Gym',
+      description: null,
+      latitude: new Decimal(userLatitude),
+      longitude: new Decimal(userLongitude),
+      phone: null,
+    })
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('should be possible to check in', async () => {
+    const { checkIn } = await sut.execute({
+      userId,
+      gymId,
+      userLatitude,
+      userLongitude,
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be possible to check in twice on the same day', async () => {
+    vi.setSystemTime(new Date(2024, 0, 1, 8, 0, 0))
+
+    await sut.execute({
+      userId,
+      gymId,
+      userLatitude,
+      userLongitude,
+    })
+
+    await expect(() =>
+      sut.execute({
+        userId,
+        gymId,
+        userLatitude,
+        userLongitude,
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+
+  it('should be possible to check in twice but on different days', async () => {
+    vi.setSystemTime(new Date(2024, 0, 1, 8, 0, 0))
+
+    await sut.execute({
+      userId,
+      gymId,
+      userLatitude,
+      userLongitude,
+    })
+
+    vi.setSystemTime(new Date(2024, 0, 2, 8, 0, 0))
+
+    const { checkIn } = await sut.execute({
+      userId,
+      gymId,
+      userLatitude,
+      userLongitude,
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be possible to check in on distant gym', async () => {
+    gymsRepository.gyms.push({
+      id: 'distant-gym',
+      title: 'JavaScript Gym',
+      description: null,
+      latitude: new Decimal(-21.7730056),
+      longitude: new Decimal(-43.3465522),
+      phone: null,
+    })
+
+    await expect(() =>
+      sut.execute({
+        userId,
+        gymId: 'distant-gym',
+        userLatitude,
+        userLongitude,
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+
+  it('should be possible to check in on near gym', async () => {
+    const { checkIn } = await sut.execute({
+      userId,
+      gymId,
+      userLatitude,
+      userLongitude,
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+})
