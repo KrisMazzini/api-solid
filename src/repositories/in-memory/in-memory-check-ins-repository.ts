9a58ee2f7cfaa283ab903b0409036af09796a7f5
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2024-11-14 18:49:27 -0300

    feat: implement 'get user metrics' use case

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -42,4 +42,8 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     return checkInOnSameDate ?? null
   }
+
+  async countByUserId(userId: string) {
+    return this.checkIns.filter((checkIn) => checkIn.user_id === userId).length
+  }
 }
